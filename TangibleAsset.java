d728bc331709d6ccbe9b79fc5018c4f3efabb552
sa0426 <setoarisa.0426@gmail.com>
2024-07-08 10:51:31 +0900

    Update TangibleAsset.java

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -8,7 +8,7 @@ public TangibleAsset(String name , int price , String color){
   this.price = price;
   this.color = color;
 }
-  public String getName(){return this.name}
-  public int getPrice(){return this.price}
-  public String getColor(){return this.color}
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
 }
